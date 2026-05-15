ab7a5550f26358abf73afaf61e7d9b28e508c3e6
MK Pau-tin <mppautin@up.edu.ph>
2026-05-15 15:12:25 +0800

    add mock server endpoint

--- GATE_Auth.py
+++ GATE_Auth.py
@@ -4,6 +4,7 @@ from mosip_auth_sdk import MOSIPAuthenticator
 from dynaconf import Dynaconf
 from pprint import pprint
 import time
+import requests
 
 from ID_Payload import ScannedIDPayload
 from database import process_entry, process_exit, process_reset 
@@ -15,9 +16,11 @@ authenticator = MOSIPAuthenticator(config=config)
 # [2] App
 app = FastAPI()
 
+DUMMY_URL = "https://cs145-iot-cup-1745973870.ap-southeast-1.elb.amazonaws.com"
+useMock = True
 
 # [3] Shared MOSIP auth helper
-def run_mosip_auth(payload: ScannedIDPayload) -> bool:
+def run_mosip_auth(payload: ScannedIDPayload, mock = False) -> bool:
     def wrap(value):
         return [{"language": "eng", "value": value}] if value else None
 
@@ -33,32 +36,42 @@ def run_mosip_auth(payload: ScannedIDPayload) -> bool:
         address_line3=wrap(payload.address_line3),
     )
 
-    # try:
-    #     response = authenticator.auth(
-    #         individual_id=payload.uin,
-    #         individual_id_type="UIN",
-    #         demographic_data=demographics_data,
-    #         consent=True,
-    #     )
-    #     return response.json().get("response", {}).get("authStatus", False)
-    # except Exception:
-    #     raise HTTPException(status_code=503, detail="Identity service unavailable")
-
     is_error = False
     for x in range(3):
         try:
-            response = authenticator.auth(
-                individual_id=payload.uin,
-                individual_id_type="UIN",
-                demographic_data=demographics_data,
-                consent=True,
-            )
+            if mock:
+                response = requests.post(
+                    f"{DUMMY_URL}/api/v1/auth/yes-no",
+                    json={
+                        "individual_id": payload.uin,
+                        "individual_id_type": "UIN",
+                        "name": payload.name,
+                        "dob": payload.dob,
+                        "location1": payload.location1,
+                        "location3": payload.location3,
+                        "zone": payload.zone,
+                        "postal_code": payload.postal_code,
+                        "address_line1": payload.address_line1,
+                        "address_line2": payload.address_line2,
+                        "address_line3": payload.address_line3,
+                        "consent": True
+                    },
+                    verify=False
+                )
+            else:
+                response = authenticator.auth(
+                    individual_id=payload.uin,
+                    individual_id_type="UIN",
+                    demographic_data=demographics_data,
+                    consent=True,
+                )
             data = response.json()
             print("MOSIP Auth request response:")
             pprint(data)
             return data.get("response", {}).get("authStatus", False)
-        except Exception:
+        except Exception as e:
             print("Identity service unavailable")
+            print(e)
             is_error = True
             time.sleep(3)
     if is_error:
@@ -70,7 +83,7 @@ def run_mosip_auth(payload: ScannedIDPayload) -> bool:
 @app.post("/enterRequest")
 async def enter_request(payload: ScannedIDPayload):
     print("QR Payload:", payload)
-    if not run_mosip_auth(payload):
+    if not run_mosip_auth(payload, mock=useMock):
         return {"authStatus": False, "status": "not in MOSIP"}
 
     db_payload = {"uin": payload.uin, "precinctID": payload.precinctID}
@@ -82,7 +95,7 @@ async def enter_request(payload: ScannedIDPayload):
 @app.post("/exitRequest")
 async def exit_request(payload: ScannedIDPayload):
     print("QR Payload:", payload)
-    if not run_mosip_auth(payload):
+    if not run_mosip_auth(payload, mock=useMock):
         return {"authStatus": False, "status": "not in MOSIP"}
 
     db_payload = {"uin": payload.uin, "precinctID": payload.precinctID}
